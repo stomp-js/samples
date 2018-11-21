62e443200dc0ad58a7ccfadc1216d75bafa44a49
deepak <deepak@kreatio.com>
2018-11-21 15:44:27 +0530

    documentation updates

--- rx-stomp/rpc/ruby-rpc-server/rpc-helper.rb
+++ rx-stomp/rpc/ruby-rpc-server/rpc-helper.rb
@@ -1,5 +1,21 @@
 require 'logger'
 
+###############################################################################
+# A sophisticated RPC server framework.
+#
+# It supports the following:
+#
+# * You can pass a thread_pool which is instance of `Thread.pool` provided
+#   by gem `thread`.
+# * If you set `manual_ack` in `subscribe_opts`, it will take care of
+#   `ack` for successful response. In case of any Exceptions, it will `reject`.
+# * It supports `logger`, you can pass an instance to customize logging.
+# * It should be considered production ready, please raise issues if you
+#   find any cases which are not covered.
+#
+# See rpc-server-thread-pool.rb for usage sample
+###############################################################################
+
 class RPCHelper
   def self.serve(opts = {}, &compute)
     end_point = opts[:end_point]
@@ -11,12 +27,13 @@ class RPCHelper
     use_thread_pool = (!!thread_pool)
     manual_ack = subscribe_opts[:manual_ack]
 
-    channel = amqp_conn.create_channel
-    queue = channel.queue(end_point)
+    main_channel = amqp_conn.create_channel
+    queue = main_channel.queue(end_point)
 
     # to serialize ack/rejects on the channel
     mutex = Mutex.new
 
+    # This has been factored out to support usage with and without a thread pool
     processor = lambda do |delivery_info, metadata, payload|
       begin
         logger.debug "Received: #{payload}"
@@ -26,8 +43,8 @@ class RPCHelper
         logger.debug "RPC Server: Response: #{response_body} for #{payload}"
 
         # Channel is not guaranteed to be thread safe
-        # Since we are in another thread, we must open a new channel
-        private_channel = amqp_conn.create_channel
+        # We must open a new channel when using thread pool
+        private_channel = use_thread_pool ? amqp_conn.create_channel : main_channel
         default_exchange = private_channel.default_exchange
 
         default_exchange.publish response_body,
@@ -40,7 +57,7 @@ class RPCHelper
           mutex.synchronize do
             # Second parameter tells only this message is acknowledged
             # (instead of all unacknowledged messages till now)
-            channel.acknowledge(delivery_info.delivery_tag, false)
+            main_channel.acknowledge(delivery_info.delivery_tag, false)
           end
         end
       rescue StandardError => exception
@@ -51,11 +68,11 @@ class RPCHelper
           # Channel is not guaranteed to be thread safe, so mutex
           mutex.synchronize do
             # The second parameter request the broker to requeue the message
-            channel.reject(delivery_info.delivery_tag, true)
+            main_channel.reject(delivery_info.delivery_tag, true)
           end
         end
 
-        private_channel.close if private_channel
+        private_channel.close if use_thread_pool && private_channel
       end
     end
 
--- rx-stomp/rpc/ruby-rpc-server/rpc-server.rb
+++ rx-stomp/rpc/ruby-rpc-server/rpc-server.rb
@@ -2,14 +2,29 @@ require 'bundler/setup'
 require 'bunny'
 require 'json'
 
-conn = Bunny.new
-conn.start
+###############################################################################
+# This demonstrates a minimal RPC server implemented in Ruby using AMQP
+# through bunny.
+#
+# The key part is sending the response back through default exchange to the
+# reply queue with appropriate correlation_id
+#
+# This may suffice for many use cases.
+#
+# For a more robust implementation please see rpc-helper.rb and
+# rpc-server-thread-pool.rb
+###############################################################################
 
-ch = conn.create_channel
-q = ch.queue("integer-addition")
 
-q.subscribe(block: true) do |delivery_info, metadata, payload|
+amqp_conn = Bunny.new
+amqp_conn.start
+
+channel = amqp_conn.create_channel
+queue = channel.queue("integer-addition")
+
+queue.subscribe(block: true) do |delivery_info, metadata, payload|
   puts "Received: #{payload}"
+
   # Process the request, compute the response
   operands = JSON.parse(payload)
   result = {result: operands['x'].to_i + operands['y'].to_i}
@@ -18,7 +33,7 @@ q.subscribe(block: true) do |delivery_info, metadata, payload|
 
   puts "RPC Server: Response: #{response_body} for #{payload}"
 
-  x = ch.default_exchange
+  default_exchange = channel.default_exchange
 
-  x.publish response_body, routing_key: metadata[:reply_to], correlation_id: metadata[:correlation_id]
+  default_exchange.publish response_body, routing_key: metadata[:reply_to], correlation_id: metadata[:correlation_id]
 end
